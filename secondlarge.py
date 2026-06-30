d31f2f8594838b7fb4816ace456d8cc63d78b90f
dasari hemavathi <dasari.hemavathi8541@gmail.com>
2026-06-30 12:26:21 +0530

    second large element in a list

--- secondlarge.py
+++ secondlarge.py
@@ -1,13 +1,17 @@
-def secondlargest(lst):
-    first=float('-inf')
-    second=float('-inf')
-    for num in lst:
-        if num>first:
-            second=first 
-            first=num 
-        elif num>second and num!=first:
-            second=num 
-    print(second)
+# def secondlargest(lst):
+#     first=float('-inf')
+#     second=float('-inf')
+#     for num in lst:
+#         if num>first:
+#             second=first 
+#             first=num 
+#         elif num>second and num!=first:
+#             second=num 
+#     print(second)
     
-lst=list(map(int,input().split()))    
-secondlargest(lst)
+# lst=list(map(int,input().split()))    
+# secondlargest(lst)
+lst=[10,20,423,420,425,53,523,90,100]
+lst.sort()
+
+print(lst[-2])
