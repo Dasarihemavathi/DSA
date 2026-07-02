4f853ffba41e04b8203692a7fc8a80f822380a7c
dasari hemavathi <dasari.hemavathi8541@gmail.com>
2026-07-02 15:28:13 +0530

    merge and missing num

---
+++ merge.py
@@ -0,0 +1,19 @@
+# list1=[1,3,5]
+# list2=[2,4,6]
+# result=list1+list2
+# print(sorted(result))
+
+# a=[3,0,1]
+ 
+# output should be : 2
+
+lst=[3,0,1]
+n=len(lst)
+
+for i in range(n+1):
+    if i not in lst:
+        print(i)
+        
+        
+ 
+ 
